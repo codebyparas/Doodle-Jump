bea088751863964e6d2790f32a8de231c452a8f8
Paras-Sachdeva <paras140902@gmail.com>
2024-05-09 22:05:17 +0530

    ADD: Canvas

---
+++ doodlejump.js
@@ -0,0 +1,11 @@
+// Board
+let board;
+let boardWidth = 360;
+let boardHeight = 576;
+let context;
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+}
